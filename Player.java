dc39de83ad1448d54299e1ec2bc9f6f9fcf2ba86
jungho-kim1 <47431757+jungho-kim1@users.noreply.github.com>
2019-02-19 12:41:54 -0700

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,21 @@
+public class Player {
+	
+	public int initialMoveX = 0;
+	public int initialMoveY = 0;
+
+	
+	public void changeMove extends movement (){
+		
+		int xPos = movementX;
+		int yPos = movementY;
+	}
+	
+	public void setPos(){
+		//positioned on a X and Y arraylist?
+		return playerCoordinate;
+	}
+	
+	//public void getMove(){
+		
+	
+}
