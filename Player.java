9e5f729567d2a298b318db58f8db7d8bccc7ebc8
jungho-kim1 <47431757+jungho-kim1@users.noreply.github.com>
2019-02-19 13:39:56 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,19 +3,27 @@ public class Player {
 	public int initialMoveX = 0;
 	public int initialMoveY = 0;
 
+	public class HumanPlayer {
 	
-	public void changeMove extends movement (){
 		
-		int xPos = movementX;
-		int yPos = movementY;
-	}
-	
-	public void setPos(){
-		//positioned on a X and Y arraylist?
-		return playerCoordinate;
-	}
-	
-	//public void getMove(){
+		public void changeMove extends movement (){
+			
+			int xPos = movementX;
+			int yPos = movementY;
+		}
 		
-	
+		public void setPos(){
+			//positioned on an X and Y arraylist?
+			return playerCoordinate;
+		}
+		
+		public ArrayList<int> getMove (){
+			
+			return movement;
+			
+		}
+		//public void getMove(){
+			
+		//token class for the GUI (gives position and card associated with each player)
+	}
 }
