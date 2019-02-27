bf85337b7ced0edfbf631febfb23b4a53d9bc3b2
jungho-kim1 <47431757+jungho-kim1@users.noreply.github.com>
2019-02-27 12:04:14 -0700

    Update Player.java
    added tokens to the class

--- Player.java
+++ Player.java
@@ -1,29 +1,48 @@
-public class Player {
+public class Player{
+	
+	//Player class organizes turns of the players but primarily returns tokens with different values
+	//includes computer (AI) players.)
 	
 	public int initialMoveX = 0;
 	public int initialMoveY = 0;
-
-	public class HumanPlayer {
 	
-		
-		public void changeMove extends movement (){
-			
-			int xPos = movementX;
-			int yPos = movementY;
-		}
-		
-		public void setPos(){
-			//positioned on an X and Y arraylist?
-			return playerCoordinate;
-		}
-		
-		public ArrayList<int> getMove (){
-			
-			return movement;
-			
-		}
+	//public void changeMove extends movement ()
 		//public void getMove(){
 			
+			//int xPos = movementX;
+			//int yPos = movementY;
+
+	
+	public class humanPlayer{
+		
+		System.out.println("Enter number of players: ");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int numPlayers = scanner.nextLine();
+		
+		String[] currentPlayers = {numPlayers};
+		
+		public static void setToken (){
+			
+			for (int i = 0; i < numPlayers; i++){
+				
+				System.out.println("Type in the token you want to play as: ");
+				System.out.println("% ! # &");
+				
+				Scanner scanner = new Scanner(System.in);
+		
+				String playerToken = scanner.nextLine();
+				
+				currentPlayers[i] = playerToken;
+			}
+			
+		}
+				
+				//assign playerToken to arraylist
+				
+			
+		}
+			
 		//token class for the GUI (gives position and card associated with each player)
-	}
 }
