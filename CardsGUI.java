d40fb2a60bd628efc2bbd7ff856456cc4e4f0dcd
hailey-allen <47430391+hailey-allen@users.noreply.github.com>
2019-03-13 17:20:11 -0600

    CardsGUI fixed for demo2 - Hailey Allen
    CardsGUI works and will now be committed to the main GUI class and/or ready to be called within it

--- CardsGUI.java
+++ CardsGUI.java
@@ -18,6 +18,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.*;
 
 public class CardsGUI extends Application{
 	
@@ -44,89 +45,168 @@ public class CardsGUI extends Application{
 //player one, card one
 		Rectangle rectangle = new Rectangle(100, 100, 100, 100);
 		Label player = new Label(playerOne.get(0));
+		player.setTextFill(Color.BLACK);
+		rectangle.setStroke(Color.WHITE);
 		rectangle.setFill(Color.BLUE);
 //player one, card two
-		Rectangle rectangle3 = new Rectangle(100, 100, 100, 100);
-		Label player3 = new Label(playerOne.get(1));
-		rectangle3.setFill(Color.BLUE);
+		Rectangle rectangle2 = new Rectangle(100, 100, 100, 100);
+		Label player2 = new Label(playerOne.get(1));
+		player2.setTextFill(Color.BLACK);
+		rectangle2.setStroke(Color.WHITE);
+		rectangle2.setFill(Color.BLUE);
 //player one, card three
-		Rectangle rectangle5 = new Rectangle(100, 100, 100, 100);
-		Label player5 = new Label(playerOne.get(2));
-		rectangle5.setFill(Color.BLUE);
+		Rectangle rectangle3 = new Rectangle(100, 100, 100, 100);
+		Label player3 = new Label(playerOne.get(2));
+		player3.setTextFill(Color.BLACK);
+		rectangle3.setStroke(Color.WHITE);
+		rectangle3.setFill(Color.BLUE);
 //player one, card four
-		Rectangle rectangle7 = new Rectangle(100, 100, 100, 100);
-		Label player7 = new Label(playerOne.get(3));
-		rectangle7.setFill(Color.BLUE);
+		Rectangle rectangle4 = new Rectangle(100, 100, 100, 100);
+		Label player4 = new Label(playerOne.get(3));
+		player4.setTextFill(Color.BLACK);
+		rectangle4.setStroke(Color.WHITE);
+		rectangle4.setFill(Color.BLUE);
 //player one, card five
-		Rectangle rectangle9 = new Rectangle(100, 100, 100, 100);
-		Label player9 = new Label(playerOne.get(4));
-		rectangle9.setFill(Color.BLUE);
+		Rectangle rectangle5 = new Rectangle(100, 100, 100, 100);
+		Label player5 = new Label(playerOne.get(4));
+		player5.setTextFill(Color.BLACK);
+		rectangle5.setStroke(Color.WHITE);
+		rectangle5.setFill(Color.BLUE);
 //player one, card six
-		Rectangle rectangle11 = new Rectangle(100, 100, 100, 100);
-		Label player11 = new Label(playerOne.get(5));
-		rectangle11.setFill(Color.BLUE);
+		Rectangle rectangle6 = new Rectangle(100, 100, 100, 100);
+		Label player6 = new Label(playerOne.get(5));
+		player6.setTextFill(Color.BLACK);
+		rectangle6.setStroke(Color.WHITE);
+		rectangle6.setFill(Color.BLUE);
 
 		//Player Two
 
 //player two, card one
-		Rectangle rectangle2 = new Rectangle(100, 100, 100, 100);
-		Label player2 = new Label(playerTwo.get(0));
-		rectangle2.setFill(Color.RED);
+		Rectangle rectangle7 = new Rectangle(10, 10, 100, 100);
+		Label player7 = new Label(playerTwo.get(0));
+		player7.setTextFill(Color.BLACK);
+		rectangle7.setStroke(Color.WHITE);
+		rectangle7.setFill(Color.PURPLE);
 //player two, card two
-		Rectangle rectangle4 = new Rectangle(100, 100, 100, 100);
-		Label player4 = new Label(playerTwo.get(0));
-		rectangle4.setFill(Color.RED);
+		Rectangle rectangle8 = new Rectangle(10, 10, 100, 100);
+		Label player8 = new Label(playerTwo.get(1));
+		player8.setTextFill(Color.BLACK);
+		rectangle8.setStroke(Color.WHITE);
+		rectangle8.setFill(Color.PURPLE);
 //player two, card three
-		Rectangle rectangle6 = new Rectangle(100, 100, 100, 100);
-		Label player6 = new Label(playerTwo.get(2));
-		rectangle6.setFill(Color.RED);
+		Rectangle rectangle9 = new Rectangle(10, 10, 100, 100);
+		Label player9 = new Label(playerTwo.get(2));
+		player9.setTextFill(Color.BLACK);
+		rectangle9.setStroke(Color.WHITE);
+		rectangle9.setFill(Color.PURPLE);
 //player two, card four
-		Rectangle rectangle8 = new Rectangle(100, 100, 100, 100);
-		Label player8 = new Label(playerTwo.get(3));
-		rectangle8.setFill(Color.RED);
+		Rectangle rectangle10 = new Rectangle(10, 10, 100, 100);
+		Label player10 = new Label(playerTwo.get(3));
+		player10.setTextFill(Color.BLACK);
+		rectangle10.setStroke(Color.WHITE);
+		rectangle10.setFill(Color.PURPLE);
 //player two, card five
-		Rectangle rectangle10 = new Rectangle(100, 100, 100, 100);
-		Label player10 = new Label(playerTwo.get(4));
-		rectangle10.setFill(Color.RED);
+		Rectangle rectangle11 = new Rectangle(10, 10, 100, 100);
+		Label player11 = new Label(playerTwo.get(4));
+		player11.setTextFill(Color.BLACK);
+		rectangle11.setStroke(Color.WHITE);
+		rectangle11.setFill(Color.PURPLE);
 //player two, card six
-		Rectangle rectangle12 = new Rectangle(100, 100, 100, 100);
+		Rectangle rectangle12 = new Rectangle(10, 10, 100, 100);
 		Label player12 = new Label(playerTwo.get(5));
-		rectangle12.setFill(Color.RED);
+		player12.setTextFill(Color.BLACK);
+		rectangle12.setStroke(Color.WHITE);
+		rectangle12.setFill(Color.PURPLE);
 
 //setting up HBox so they will all be beside one another
 		StackPane stack = new StackPane();
 		StackPane stack2 = new StackPane();
+		StackPane stack3 = new StackPane();
+		StackPane stack4 = new StackPane();
+		StackPane stack5 = new StackPane();
+		StackPane stack6 = new StackPane();
+		StackPane stack7 = new StackPane();
+		StackPane stack8 = new StackPane();
+		StackPane stack9 = new StackPane();
+		StackPane stack10 = new StackPane();
+		StackPane stack11 = new StackPane();
+		StackPane stack12 = new StackPane();
 
 		HBox h = new HBox();
+		HBox h2 = new HBox();
+		VBox v = new VBox();
 
 		FlowPane flow = new FlowPane();
-		flow.setVgap(5);
-		flow.setHgap(5);
-
 		FlowPane flow2 = new FlowPane();
-		flow2.setVgap(8);
-		flow2.setHgap(8);
-
 		FlowPane flow3 = new FlowPane();
-		flow3.setVgap(5);
-		flow3.setHgap(5);
-
 		FlowPane flow4 = new FlowPane();
-		flow4.setVgap(8);
-		flow4.setHgap(8);
+		FlowPane flow5 = new FlowPane();
+		FlowPane flow6 = new FlowPane();
+		FlowPane flow7 = new FlowPane();
+		FlowPane flow8 = new FlowPane();
+		FlowPane flow9 = new FlowPane();
+		FlowPane flow10 = new FlowPane();
+		FlowPane flow11 = new FlowPane();
+		FlowPane flow12 = new FlowPane();
 
-		flow.getChildren().addAll(rectangle, rectangle2, rectangle3, rectangle4, rectangle5, rectangle6);
-		flow2.getChildren().addAll(player, player2, player3, player4, player5, player6);
+		FlowPane play = new FlowPane();
+		FlowPane play2 = new FlowPane();
+		FlowPane play3 = new FlowPane();
+		FlowPane play4 = new FlowPane();
+		FlowPane play5 = new FlowPane();
+		FlowPane play6 = new FlowPane();
+		FlowPane play7 = new FlowPane();
+		FlowPane play8 = new FlowPane();
+		FlowPane play9 = new FlowPane();
+		FlowPane play10 = new FlowPane();
+		FlowPane play11 = new FlowPane();
+		FlowPane play12 = new FlowPane();
 		
-		flow3.getChildren().addAll(rectangle7, rectangle8, rectangle9, rectangle10, rectangle11, rectangle12);
-		flow4.getChildren().addAll(player7, player8, player9, player10, player11, player12);
+		flow.getChildren().add(rectangle);
+		flow2.getChildren().add(rectangle2);
+		flow3.getChildren().add(rectangle3);
+		flow4.getChildren().add(rectangle4);
+		flow5.getChildren().add(rectangle5);
+		flow6.getChildren().add(rectangle6);
+		flow7.getChildren().add(rectangle7);
+		flow8.getChildren().add(rectangle8);
+		flow9.getChildren().add(rectangle9);
+		flow10.getChildren().add(rectangle10);
+		flow11.getChildren().add(rectangle11);
+		flow12.getChildren().add(rectangle12);
 
-		stack.getChildren().addAll(flow, flow2);
-		stack2.getChildren().addAll(flow3, flow4);
+		play.getChildren().add(player);
+		play2.getChildren().add(player2);
+		play3.getChildren().add(player3);
+		play4.getChildren().add(player4);
+		play5.getChildren().add(player5);
+		play6.getChildren().add(player6);
+		play7.getChildren().add(player7);
+		play8.getChildren().add(player8);
+		play9.getChildren().add(player9);
+		play10.getChildren().add(player10);
+		play11.getChildren().add(player11);
+		play12.getChildren().add(player12);
 
-		h.getChildren().addAll(stack, stack2);
+		stack.getChildren().addAll(flow, play);
+		stack2.getChildren().addAll(flow2, play2);
+		stack3.getChildren().addAll(flow3, play3);
+		stack4.getChildren().addAll(flow4, play4);
+		stack5.getChildren().addAll(flow5, play5);
+		stack6.getChildren().addAll(flow6, play6);
+		stack7.getChildren().addAll(flow7, play7);
+		stack8.getChildren().addAll(flow8, play8);
+		stack9.getChildren().addAll(flow9, play9);
+		stack10.getChildren().addAll(flow10, play10);
+		stack11.getChildren().addAll(flow11, play11);
+		stack12.getChildren().addAll(flow12, play12);
 
-		Scene scene = new Scene(h, 500, 500);
+		h.getChildren().addAll(stack, stack2, stack3, stack4, stack5, stack6);
+		h2.getChildren().addAll(stack7, stack8, stack9, stack10, stack11, stack12);
+
+		v.getChildren().addAll(h, h2);
+
+		Scene scene = new Scene(v, 500, 500);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
