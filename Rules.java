0d1bbf8ef919cb4e3f02fc3e66766d2c793edf6e
Kylie Sicat <47333417+kg-sicat@users.noreply.github.com>
2019-03-14 00:40:23 -0600

    Changed and simplified the rules for Demo 2

--- Rules.java
+++ Rules.java
@@ -1,102 +1,22 @@
+/**
+* Rules class that contains the rules for the board game clue
+* @author Kylie Sicat
+* @since 03-14-19
+*/
+
+
 public class Rules {
+	
+	public Rules(){}
+	
 	//println(  + )
-	public static void main(String[] args) {
+	public void printRules() {
 
 		System.out.println("Welcome To Clue!");
-		//System.out.println("Rules:\nFor the First Demo there are only 2 players the Human & Computer");
-		
-		// Equipment
-		System.out.println("Equipment:\nSix colored tokens representing the suspects in the house.\nThe colors of the pieces are closely associated with the names of the suspects: \nMiss Scarlet     (red) \nProfessor Plum      (purple) \nMr. Green     (green) \nMrs. White    (white)");
-		System.out.println("4 miniature weapons and one die.");
-		System.out.println("The pack of 21 illustrated cards includes a card for each of the 6 suspects, one for each of the 6 weapons, and one for each of the 9 rooms. There is also a pad of Detective Notes to aid the players in their investigations.");
-		System.out.println("The game Board showing nine rooms of Mr. Boddy's house.");
-		
-		// Preparation
-		System.out.println("Preparation\n1.  Place the suspect tokens on the starting squares marked with their names. All 6 pieces are placed on the board regardless of the number of  players.\n2.  Place each of the Weapons in a different room using any of the rooms.\n3.  Place the empty envelope marked 'Case File CONFIDENTIAL' on the spot marked 'X' in the center of the board.\n4.  Sort the pack of cards into three groups- Room Cards, Weapon Cards and Suspect Cards. Shuffle each of these three groups separately. Take the top card from each group and place it in the envelope. This should be done carefully so that no player knows any of the three cards (one room, one weapon, and one suspect) placed in the envelope.\n5.  Shuffle together the three piles of remaining cards. Then deal them face down clockwise around the table. It is important that no player shall see any of the cards while they are being shuffled and dealt. (It doesn't matter if some players receive more cards than others.)   Secretly look at your own cards:  Because they're in your hand, they can't be in the Case File - which means that none of your cards was involved in the crime.\n6.  Take a detective's notebook sheet and, so that no one can see what you write, fold it in half.  Check off the cards that are in your hand.\n7.  Each player takes the colored token nearest to him on the board, and uses it throughout the game. The player having the red token, Miss Scarlet,  rolls the die and moves first. After Miss Scarlet has moved, the next player on the left rolls the die and moves. Each of the other players follows in turn.");
-		//
-		/**Equipment: 
-
- Preparation:
-
-"1.  Place the suspect tokens on the starting squares marked with their names. All 6 pieces are placed on the board regardless of the number of  players.\n2.  Place each of the Weapons in a different room using any of the rooms.\n3.  Place the empty envelope marked "Case File CONFIDENTIAL" on the spot marked "X" in the center of the board.\n4.  Sort the pack of cards into three groups- Room Cards, Weapon Cards and Suspect Cards. Shuffle each of these three groups separately. Take the top card from each group and place it in the envelope. This should be done carefully so that no player knows any of the three cards (one room, one weapon, and one suspect) placed in the envelope.\n5.  Shuffle together the three piles of remaining cards. Then deal them face down clockwise around the table. It is important that no player shall see any of the cards while they are being shuffled and dealt. (It doesn't matter if some players receive more cards than others.)   Secretly look at your own cards:  Because they're in your hand, they can't be in the Case File - which means that none of your cards was involved in the crime.\n6.  Take a detective's notebook sheet and, so that no one can see what you write, fold it in half.  Check off the cards that are in your hand.\n7.  Each player takes the colored token nearest to him on the board, and uses it throughout the game. The player having the red token, Miss Scarlet,  rolls the die and moves first. After Miss Scarlet has moved, the next player on the left rolls the die and moves. Each of the other players follows in turn."
-
-Game Play:
-
-Moving your token:
-
-On each turn, try to reach a different room of the mansion.  To start your turn, move your token either by rolling the die or, if you're in a corner room, using a Secret Passage.
-
-There are three ways of entering a room: 
- (1) Throwing the die and moving your token along the squares entering through a doorway, 
- (2) Via the Secret Passages by leaping across the board, corner to corner, without using the die, and 
- (3) a player's token may be placed in a room by another player in the feature play known as "The Suggestion." If the space at the entrance to a room is occupied 
- by the token of one player, no other player may move into that room, through that door.
-
- Getting out of a room: There are three ways of leaving a room: 
- (1) by throwing the die and moving out through a doorway onto the squares, heading toward another room of your choice, 
- (2) by using the Secret Passages and finally, 
- (3) by being transferred to a new room by some other player.
-
- On the throw of the die, you may enter a Room by the doors only, but you cannot leave a room on the same turn.  Entering the Room ends your move. It is not 
- necessary to throw the exact number to enter a Room. That is, if you need 4 to get into a room and you have thrown a 6, ignore the last two units after entering 
- the Room.
-
-Players already in a room may leave it by any door using the die as usual and moving toward another room or, they may use a secret passage, if in a corner room. The doors of each room are not counted as a square.
-
- Secret Passages: 
-The Secret Passages shown in the corner rooms enable players to move between opposite corner rooms in one move. This can be done on a  player's turn without throwing the die merely by moving his token to the opposite corner room and announcing he is using the Secret Passage. A Suggestion may be made after this move.
-
-Making a Suggestion:
-
-As soon as you enter a room, make a Suggestion. By making Suggestions, you try to determine - by process of elimination -- which three cards are in the confidential Case File envelope.  To make a Suggestion, move a Suspect and a Weapon into the Room that you just entered.  Then suggest that the crime was committed in that Room, by that Suspect, with that Weapon.
-
-Example:  Let's say that you're Miss Scarlet and you enter the Lounge. First move another Suspect -- Mr. Green, for instance --  into the Lounge.  Then move a Weapon into the Lounge (the Wrench, perhaps) and say "I suggest that the crime was committed in the Lounge, by Mr. Green with the Wrench."
-
-Remember two things: 
-   1.  You must be in the Room that you mention in your Suggestion. 
-   2.  Be sure to consider all tokens -- including spare Suspects and yourself! -- as falling under equal suspicion.
-
- Proving the suggestion true or false:
-
-As soon as you make a Suggestion, your opponents, in turn, try to prove it false. The first to try is the player to your immediate left.  This player looks at his or her cards to see if one of the three cards you just named is there.  If the player has one or more of these cards, he or she must show it to you and no one else.  If the player has more than one of the cards named, he or she select just one to show you.
-
- If that opponent does not have any of the three cards, then the next player at his left examines his cards and must show one of the three if he has it. Obviously, if any player has one or more of the 3 cards named in the Suggestion, it is proof that those particular cards are not in the envelope.
-
- The opportunity to prove the Suggestion false passes to the left until some player has shown ONE card to the suggesting player, whose turn then ends, and play 
- passes to the next player. If no one is able to prove your Suggestion false, you may either end your turn or make an Accusation now.
-
-Special Notes about Suggestions 
-You may make only one Suggestion after entering a particular Room.  To make your next Suggestion, you must either enter a different Room or sometime after your next turn, re-enter the Room that you most recently left.  You may not forfeit a turn to remain in a particular Room.
-
- If yours was the Suspect moved into a room by a Suggestion, you may, on your next turn, make a Suggestion of your own for that room.  If you decide to make a Suggestion, do not roll the die or move your token.
-
- By making Suggestions and having them proved True or False players will eventually be able to identify the three cards in the envelope.
-
- Tokens and weapons transferred to a room as the result of a Suggestion are not returned to their original positions on the board.  There is no limit to the number of Suspect tokens or Weapons that may be in a Room at one time.
-
- To leave a room in which your token has been placed by a Suggestion, either roll the die or, if you're in a corner room, you can use the Secret Passage.
-
- Although there is no requirement or rule on how players should use the Detective Note Pads it is suggested that the best and easiest way to play the game is to 
- check off items on the Note Pads as they become known.  Some players find it helpful to mark the initials of the player who showed the card.
-
-Making an Accusation: 
-When you think you've figured out which three cards are in the envelope, you may, on your turn, make an Accusation.  First say "I accuse (Suspect) of committing the crime in the (Room) with the (Weapon)"  Then, so that the other players do not see, look at the three cards in the envelope.
-
-In a Suggestion, the Room you name must be the Room where your token is located.  But in an Accusation, you may name any Room. 
-  
-Winning the game: 
-If the Accusation is completely correct, that is, if you find in the envelope, all 3 cards that you just named, lay the cards face up on the table and you are the winner.
-
-If the Accusation is incorrect 
-Secretly return the three cards to the envelope replace it on the board. 
-
-You may make no further moves in the game.
-
-You remain as a player to contradict Suggestions made by other players.
-
-If your token is blocking a door, move it into Room so that other players may enter.
-
-Your opponents may continue to move your token into the various Rooms where they make Suggestions. */
-
+		System.out.println("Rules:\nFor the Second Demo there are only 2 Human players");
+		System.out.println("The objective of the game is to access the room in which the murder was set placed");
+		System.out.println("You must click the button 'Roll Dice' and gives you the allow amount of spaces you can move for your turn, once your moves left is equal to zero you must click the Button 'End turn' and let the other player repeat.");
+		System.out.println("Once the command line prompt prints that you have found the room the murder was set placed you have won the game.");
+	
 	}
 }
