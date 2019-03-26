b7ffd41a40086552ba5e2bd7b84a92d11b9a690b
Kylie Sicat <47333417+kg-sicat@users.noreply.github.com>
2019-03-26 14:28:34 -0600

    Add files via upload

--- Accusation.java
+++ Accusation.java
@@ -6,8 +6,19 @@
 
 public class Accusation extends Suspicion {
 	
+	public Accusation(Weapon weapon, Room room, Person person) {
+		super(weapon, room, person);
+	}
 
+	public void checkWinner() {
+	/**	if (accusation == winningCards) {
+			return "You win";
+		} else {
+			return "your accusation is incorrect you lose"
+		}
+	}*/
 	// method to check if all yes or if one is incorrect they lose eg. checkWinner
+	
 
 	
 
