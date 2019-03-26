e163fda2ac227b62c17fb65a0d0e7288f5f2891e
Kylie Sicat <47333417+kg-sicat@users.noreply.github.com>
2019-03-25 19:35:56 -0600

    added temp rules for boardGUI to work

---
+++ Rules.java
@@ -0,0 +1,22 @@
+/**
+* Rules class that contains the rules for the board game clue
+* @author Kylie Sicat
+* @since 03-14-19
+*/
+
+
+public class Rules {
+	
+	public Rules(){}
+	
+	//println(  + )
+	public void printRules() {
+
+		System.out.println("Welcome To Clue!");
+		System.out.println("Rules:\nFor the Second Demo there are only 2 Human players");
+		System.out.println("The objective of the game is to access the room in which the murder was set placed");
+		System.out.println("You must click the button 'Roll Dice' and gives you the allow amount of spaces you can move for your turn, once your moves left is equal to zero you must click the Button 'End turn' and let the other player repeat.");
+		System.out.println("Once the command line prompt prints that you have found the room the murder was set placed you have won the game.");
+	
+	}
+}
